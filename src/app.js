4f41840a47ae175e387b82a7ce13099a8c3b1a64
Nguyễn Hoàng Nam <nguyenhoangnam.dev@gmail.com>
2020-10-15 18:16:00 +0700

    Prepare views _ Add editor config to overwrite default setting of vsc _ Add .env in .gitignore _ Add ejs in middleware _ Add morgan to log more information about request and response _ Add nodemon to watch js and ejs file

--- src/app.js
+++ src/app.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
-const bodyParser = require('body-parser');
-const auth = require('./api/auth');
-const user = require('./api/user');
+const job = require('./api/job');
+// const auth = require("./api/auth");
+// const user = require("./api/user");
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const path = require('path');
+const morgan = require('morgan');
 
 dotenv.config();
 const _env = process.env;
 const app = express();
-app.use(bodyParser.json());
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+// Log request and response
+app.use(morgan('dev'));
+
+app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(_env.DEV_DB, {useNewUrlParser: true})
-    .then(console.log('Connected to MongoDB'))
-    .catch(err => console.log(err));
+mongoose
+    .connect(_env.DEV_DB, {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .catch((err) => console.log(err));
 
 let port = _env.PORT || _env.DEV_PORT;
-app.use('/', auth, user);
+app.use('/', job);
 
-app.listen(port, () => { console.log('Server started on port ' + port); });
+app.listen(port, () => {});
--- src/middleware/auth/authenticate.js
+++ src/middleware/auth/authenticate.js
@@ -1,22 +1,25 @@
-const jwt = require('jsonwebtoken');
-const { options } = require('../../config/jwtCfg');
+const jwt = require("jsonwebtoken");
+const { options } = require("../../config/jwtCfg");
 
-module.exports = authenticate = (req, res, next) => {
-    const token = req.body.token ||
-                req.query.token ||
-                req.headers['x-access-token'] ||
-                req.cookies.token;
+const authenticate = (req, res, next) => {
+    const token =
+        req.body.token ||
+        req.query.token ||
+        req.headers["x-access-token"] ||
+        req.cookies.token;
 
     if (!token) {
-        res.status(401).send('No token provided');
+        res.status(401).send("No token provided");
         return;
     }
 
-    jwt.verify(token, process.env.SECRET, options(), err => {
+    jwt.verify(token, process.env.SECRET, options(), (err) => {
         if (err) {
-            res.status(401).send('Invalid token');
+            res.status(401).send("Invalid token");
             return;
         }
         next();
     });
 };
+
+module.exports = authenticate;
