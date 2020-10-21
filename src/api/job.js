e9bcd39419a385e45475a270e7ded054a51f0325
thaGitHubUser <brian42storm@gmail.com>
2020-10-22 00:05:14 +0700

    Add job get, post

--- src/api/job.js
+++ src/api/job.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require('../middleware/auth/isLoggedIn');
+const jobService = require('../components/job/jobService');
+const userService = require('../components/user/userService');
 
-router.get('/', (req, res) => {
-    res.render('main');
+router.get('/job', async (req, res) => {
+    const jobs = await jobService.getAllJobs();
+    res.json(jobs);
 });
+
+router.get('/job/:id', async (req, res) => {
+    const job = await jobService.getJobById(req.params.id);
+    res.json(job);
+});
+
+router.post('/job', isLoggedIn, async (req, res) => {
+    const {
+        recruiterId,
+        title,
+        description,
+        expiration,
+        tags
+    } = req.body;
+
+    const newJob = await jobService.createJob(
+        recruiterId,
+        title,
+        description,
+        expiration,
+        tags
+    );
+
+    // update user profile
+    let user;
+    if (newJob) {
+        user = await userService.addPostedJob(recruiterId, newJob._id, title);
+    }
+    res.json(newJob && user);
+});
+
+// router.put('/job/:id', isLoggedIn, async (req, res) => {
+//     const job = await jobService.getJobById(req.params.id);
+//     if (job) {
+//         jobService.editJob(job._id, title, description, expiration, tags);
+//     }
+
+//     res.json(job);
+// });
 
 module.exports = router;
---
+++ src/components/job/jobService.js
@@ -0,0 +1,35 @@
+const Job = require('./jobModel');
+
+module.exports = {
+    getAllJobs: () => {
+        const jobs = Job.find({});
+        return jobs ? jobs : null;
+    },
+
+    getJobById: id => {
+        const job = Job.findOne({_id: id});
+        return job ? job : null;
+    },
+
+    createJob: async (recruiterId, title, description, expiration, tags) => {
+        const job = await Job.create({
+            recruiterId,
+            title,
+            description,
+            createDate: Date(),
+            expiration,
+            tags
+        });
+        return job ? job : null;
+    },
+
+    // editJob: (id, title, description, expiration, tags) => {
+    //     Job.updateOne({_id: id}, {
+    //         title,
+    //         description,
+    //         expiration,
+    //         tags
+    //     });
+    //     return true;
+    // }
+};
