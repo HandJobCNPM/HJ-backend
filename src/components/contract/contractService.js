976786c19cdf78c5face83dfdb957f3986dabccb
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-01-06 18:58:54 +0700

    Change contract model

--- src/components/contract/contractService.js
+++ src/components/contract/contractService.js
@@ -2,94 +2,31 @@ const jobModel = require('../job/jobModel');
 const Contract = require('./contractModel');
 
 module.exports = {
-    getAllContract: () => {
-        const contracts = Contract.find({});
-        return contracts ? contracts : null;
-    },
-    getContractBytitle: title => {
-        /*
-        if (!title) {
-            return null;
-        }*/
+    getContractById: _id => {
         let errors = [];
-        if (!title) {
+        if (!_id) {
             errors.push('Error occured cause title is emmpty.');
             return errors;
         }
-        const contract = Contract.findOne({ _title: title });
+        const contract = Contract.findOne({ _id });
         if (!contract) {
             errors.push('Error occured when get contract.');
         }
         return errors.length > 0 ? errors : contract;
     },
 
-    editContract: (title, description, salary, tags, recruiterId, freelancerId, acceptDate, finished) => {
-        let errors = [];
-        if (!title || !description || !salary || !tags || !recruiterId || !freelancerId || !acceptDate || !finished) {
-            if (!title) {
-                errors.push('Error occured cause title is empty.');
-            }
-            if (!description) {
-                errors.push('Error occured cause description is empty.');
-            }
-            if (!salary) {
-                errors.push('Error occured cause salary is empty.');
-            }
-            if (!tags) {
-                errors.push('Error occured cause tag is empty.');
-            }
-            if (!recruiterId) {
-                errors.push('Error occured cause recruiter id is empty.');
-            }
-            if (!freelancerId) {
-                errors.push('Error occured cause freelancer id is empty.');
-            }
-            if (!acceptDate) {
-                errors.push('Error occured cause accept date is empty.');
-            }
-            if (finished === null) {
-                errors.push('Error occured cause finished is empty.');
-            }
-            return errors;
-        }
-        Contract.updateOne({ _title: title }, {
-            title,
-            description,
-            salary,
-            tags,
-            recruiterId,
-            freelancerId,
-            acceptDate,
-            finished
-        });
-
-        return true;
-    },
-
-    createContract: async (title, description, salary, tags, recruiterId, freelancerId, acceptDate, finished) => {
-        if (!title || !description || !salary || !tags || !recruiterId || !freelancerId || !acceptDate || !finished) {
+    createContract: async (salary, recruiterId, freelancerId, acceptDate) => {
+        if (!salary || !recruiterId || !freelancerId || !acceptDate) {
             return null;
         }
 
         const contract = await Contract.create({
-            title,
-            description,
             salary,
-            tags,
             recruiterId,
             freelancerId,
             acceptDate: Date(),
-            finished
         });
 
         return contract ? contract : null;
     },
-
-    deleteContract: title => {
-        if (!title) {
-            return false;
-        }
-        jobModel.deleteOne({_title: title});
-        return true;
-    }
 };
